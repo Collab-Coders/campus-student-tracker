08ab59efa9b7584fd2fce978bae15130fbf19d1f
Donna-Sayos <Donnasayos@gmail.com>
2026-07-05 18:28:04 -0400

    feat(api health check and script): updated the script to work on render and added a health check api

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,8 +8,15 @@ import studentRoutes from './routes/student.js';
 
 const app = express();
 
-app.use(cors()); // @TODO: update with the deployed FE URL
+app.use(cors({
+  origin: process.env.FRONTEND_ORIGIN ?? "http://localhost:5173" // @TODO: add VERCEL FE URL
+}));
 app.use(express.json());
+
+// --- HEALTH CHECK ---
+app.get("/api/health", (_req, res) => {
+  res.json({ status: "ok" });
+});
 
 // --- CAMPUS ROUTES ---
 app.use('/api/campuses', campusRoutes);
@@ -17,4 +24,5 @@ app.use('/api/campuses', campusRoutes);
 // --- STUDENT ROUTES ---
 app.use('/api/students', studentRoutes);
 
-app.listen(8080, () => console.log('Server running on port 8080'));
+const PORT = Number(process.env.PORT ?? 8080); // Render PORT or default 8080
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
